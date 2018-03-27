144e891aa427778ccfdf30708014b2b4806597ed
Leerane <leeranez@gmail.com>
2018-03-27 16:39:28 +0300

    Исправления

---
+++ js/google-map.js
@@ -0,0 +1,17 @@
+var sedonaMap = document.querySelector(".sedona-map");
+
+function initMap() {
+	var map = new google.maps.Map(sedonaMap, {
+		zoom: 9,
+		center: {lat: 34.76, lng: -111.74},
+		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.roadmap
+	});
+
+	var marker = new google.maps.Marker({
+		map: map,
+		draggable: false,
+		animation: google.maps.Animation.DROP,
+		position: {lat: 34.88, lng: -111.76}
+	});
+}
